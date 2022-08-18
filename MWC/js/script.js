16cf2cf95c81356dcb123ec25a3591cf80d944a5
worldofhope <n.sadova98@gmail.com>
2022-08-18 19:01:13 +0600

    update js

--- MWC/js/script.js
+++ MWC/js/script.js
@@ -176,7 +176,7 @@ $(document).ready(function () {
 
 //tabs
 $(function() {
-  
+
   $('ul.history__tabs__caption').on('click', 'li:not(.active)', function() {
     $(this)
       .addClass('active').siblings().removeClass('active')
