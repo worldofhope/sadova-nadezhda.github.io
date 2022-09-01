f30006eb0ed8659f29b3742d2e99c810b3a3a948
worldofhope <n.sadova98@gmail.com>
2022-09-01 13:43:02 +0600

    update sensata

--- Sensata/js/script.js
+++ Sensata/js/script.js
@@ -236,3 +236,19 @@ $(document).ready(function() {
       $(".filter-btn").addClass('active')
   });
 });
+
+// input mask tel
+$.fn.setCursorPosition = function(pos) {
+  if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos)
+  } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange()
+      range.collapse(true)
+      range.moveEnd('character', pos)
+      range.moveStart('character', pos)
+      range.select()
+  }
+}
+$('input[type="tel"]').click(function() {
+  $(this).setCursorPosition(3);
+}).mask('+7 (999) 999 99 99');
