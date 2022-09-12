1874102b53b4f4419ddff6fb88e313cde46750bb
worldofhope <n.sadova98@gmail.com>
2022-09-12 17:03:46 +0600

    update Roza

--- RosaBaglanova/js/script.js
+++ RosaBaglanova/js/script.js
@@ -118,3 +118,40 @@ $('.public__slider').slick({
       }
   ]
 });
+
+
+/*Fancybox*/
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  dragToClose: false,
+
+  Toolbar: false,
+  closeButton: "top",
+
+  Image: {
+    zoom: false,
+  },
+
+  on: {
+    initCarousel: (fancybox) => {
+      const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+
+      fancybox.$container.style.setProperty(
+        "--bg-image",
+        `url("${slide.$thumb.src}")`
+      );
+    },
+    "Carousel.change": (fancybox, carousel, to, from) => {
+      const slide = carousel.slides[to];
+
+      fancybox.$container.style.setProperty(
+        "--bg-image",
+        `url("${slide.$thumb.src}")`
+      );
+    },
+  },
+});  
+
+document.addEventListener('contextmenu', e => {
+  e.preventDefault();
+});
